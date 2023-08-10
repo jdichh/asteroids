6b85f1f144862725850d6272d2d76741de0297a6
jdichh <jpsdichoso21@gmail.com>
2023-08-10 18:02:44 +0800

    adding WASD event listeners

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ CANVAS.width = window.innerWidth;
 CANVAS.height = window.innerHeight;
 
 // Apply gradient to background
-var gradient = CONTEXT.createLinearGradient(0, 0, 0, CANVAS.width, CANVAS.height);
+var gradient = CONTEXT.createLinearGradient(0, 0, CANVAS.width, CANVAS.height);
 gradient.addColorStop(0, '#0F212A');
-gradient.addColorStop(0.50, '#0A151B');
-gradient.addColorStop(1, '#050D0D');
+gradient.addColorStop(0.99, '#050D0D');
 
 CONTEXT.fillStyle = gradient;
 CONTEXT.fillRect(0, 0, CANVAS.width, CANVAS.height);
@@ -20,8 +19,13 @@ class Player {
   }
 
   show() {
+    // just checking the center position
+    // CONTEXT.arc(this.coordinates.x, this.coordinates.y, 5, 0, Math.PI * 2, false)
+    // CONTEXT.fillStyle = 'yellow'
+    // CONTEXT.fill()
+
     CONTEXT.beginPath();
-    CONTEXT.moveTo(this.coordinates.x, this.coordinates.y - 20); // Top of the spaceship
+    CONTEXT.moveTo(this.coordinates.x, this.coordinates.y - 25); // Top of the spaceship
     CONTEXT.lineTo(this.coordinates.x - 15, this.coordinates.y + 10); // Bottom left
     CONTEXT.lineTo(this.coordinates.x, this.coordinates.y); // Middle bottom
     CONTEXT.lineTo(this.coordinates.x + 15, this.coordinates.y + 10); // Bottom right
@@ -44,4 +48,19 @@ const player = new Player({
 
 player.show();
 
-// console.log(player);
+window.addEventListener('keydown', (e) => {
+    switch (e.code) {
+        case 'KeyW':
+            alert('W key.')
+        break;
+        case 'KeyA':
+            alert('A key.')
+        break;
+        case 'KeyS':
+            alert('S key.')
+        break;
+        case 'KeyD':
+            alert('D key.')
+        break;
+    }
+})
