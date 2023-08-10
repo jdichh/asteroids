0c6a31bcc81d94cd67f4cbbb7387ad33154dd970
jdichh <jpsdichoso21@gmail.com>
2023-08-10 18:19:20 +0800

    initialization of controls

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+///// Canvas Setup /////
 const CANVAS = document.getElementById("canvas");
 const CONTEXT = CANVAS.getContext("2d");
 
 CANVAS.width = window.innerWidth;
 CANVAS.height = window.innerHeight;
 
-// Apply gradient to background
 var gradient = CONTEXT.createLinearGradient(0, 0, CANVAS.width, CANVAS.height);
-gradient.addColorStop(0, '#0F212A');
-gradient.addColorStop(0.99, '#050D0D');
+gradient.addColorStop(0, "#0F212A");
+gradient.addColorStop(0.99, "#050D0D");
+///// End of Canvas Setup /////
 
-CONTEXT.fillStyle = gradient;
-CONTEXT.fillRect(0, 0, CANVAS.width, CANVAS.height);
-
+///// Player Setup /////
 class Player {
   constructor({ coordinates, velocity }) {
     this.coordinates = coordinates;
@@ -31,13 +30,19 @@ class Player {
     CONTEXT.lineTo(this.coordinates.x + 15, this.coordinates.y + 10); // Bottom right
     CONTEXT.closePath();
 
-    CONTEXT.shadowColor = 'rgba(179, 201, 198, 1)'; // Pale turquoise of some sort
+    CONTEXT.shadowColor = "rgba(179, 201, 198, 1)"; // Pale turquoise of some sort
     CONTEXT.shadowBlur = 5; // Blur effect
     CONTEXT.shadowOffsetX = 0; // Horizontal distance
     CONTEXT.shadowOffsetY = 0; // Vertical distance
 
-    CONTEXT.strokeStyle = 'white';
-    CONTEXT.stroke()
+    CONTEXT.strokeStyle = "white";
+    CONTEXT.stroke();
+  }
+
+  updatePlayer() {
+    this.show();
+    this.coordinates.x += this.velocity.x;
+    this.coordinates.y += this.velocity.y;
   }
 }
 
@@ -45,22 +50,39 @@ const player = new Player({
   coordinates: { x: CANVAS.width / 2, y: CANVAS.height / 2 },
   velocity: { x: 0, y: 0 },
 });
+///// End of Player Setup /////
 
-player.show();
+///// Movement & Controls /////
+const KEYPRESS = {
+  w_key: {
+    pressed: false,
+  },
+};
 
-window.addEventListener('keydown', (e) => {
-    switch (e.code) {
-        case 'KeyW':
-            alert('W key.')
-        break;
-        case 'KeyA':
-            alert('A key.')
-        break;
-        case 'KeyS':
-            alert('S key.')
-        break;
-        case 'KeyD':
-            alert('D key.')
-        break;
-    }
-})
+function movement() {
+  CONTEXT.fillStyle = gradient;
+  CONTEXT.fillRect(0, 0, CANVAS.width, CANVAS.height);
+  window.requestAnimationFrame(movement);
+  player.updatePlayer();
+  
+  if (KEYPRESS.w_key.pressed) player.velocity.y = -1;
+}
+
+movement();
+
+window.addEventListener("keydown", (e) => {
+  switch (e.code) {
+    case "KeyW":
+      KEYPRESS.w_key.pressed = true;
+      break;
+    case "KeyA":
+      alert("A key.");
+      break;
+    case "KeyS":
+      alert("S key.");
+      break;
+    case "KeyD":
+      alert("D key.");
+      break;
+  }
+});
