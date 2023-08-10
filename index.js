9024e0ef70a4dd0a1ff313f6326040f802ee066e
jdichh <jpsdichoso21@gmail.com>
2023-08-10 17:04:49 +0800

    initial setup done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert('Hello!')
+const CANVAS = document.getElementById('canvas')
+const CANVAS_CONTEXT = CANVAS.getContext('2d')
+
+CANVAS.width = window.innerWidth;
+CANVAS.height = window.innerHeight;
+
+CANVAS_CONTEXT.fillRect(0, 0, CANVAS.width, CANVAS.height)
