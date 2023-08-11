c9e0d2b7e48a868928c22c30e6b1c97802096390
jdichh <jpsdichoso21@gmail.com>
2023-08-11 13:48:04 +0800

    game now detects if an asteroid hits player

--- index.js
+++ index.js
@@ -45,6 +45,26 @@ class Player {
     this.coordinates.x += this.velocity.x;
     this.coordinates.y += this.velocity.y;
   }
+
+  getVertices() {
+    const cos = Math.cos(this.rotation)
+    const sin = Math.sin(this.rotation)
+
+    return [
+      {
+        x: this.coordinates.x + cos * 30 - sin * 0,
+        y: this.coordinates.y + sin * 30 + cos * 0,
+      },
+      {
+        x: this.coordinates.x + cos * -10 - sin * 10,
+        y: this.coordinates.y + sin * -10 + cos * 10,
+      },
+      {
+        x: this.coordinates.x + cos * -10 - sin * -10,
+        y: this.coordinates.y + sin * -10 + cos * -10,
+      },
+    ]
+  }
 }
 
 class Projectile {
@@ -184,6 +204,11 @@ function updateAsteroids() {
   for (let i = ASTEROIDS.length - 1; i >= 0; i--) {
     const asteroid = ASTEROIDS[i];
     asteroid.updateAsteroid();
+
+    if(circleTriangleCollision(asteroid, player.getVertices())){
+      console.log("over")
+    }
+
     // Remove the asteroid if it's out of bounds (Garbage collector).
     if (
       asteroid.coordinates.x < 0 ||
@@ -216,17 +241,6 @@ function detectCollisions() {
 
     for (let j = ASTEROIDS.length - 1; j >= 0; j--) {
       const ASTEROID = ASTEROIDS[j];
-
-      const checkPlayerCollision = Math.sqrt(
-        (player.coordinates.x - ASTEROID.coordinates.x) ** 2 +
-        (player.coordinates.y - ASTEROID.coordinates.y) ** 2
-      );
-      console.log(checkPlayerCollision)
-
-      if (checkPlayerCollision < player.radius + ASTEROID.radius) {
-        alert('DEAD')
-      }
-
       // Calculate the distance between the projectile and asteroid.
       const distance = Math.sqrt(
         (PROJECTILE.coordinates.x - ASTEROID.coordinates.x) ** 2 +
@@ -259,6 +273,53 @@ function detectCollisions() {
   }
 }
 //// End of Projectile to Asteroid Hit Detection /////
+
+function circleTriangleCollision(circle, triangle) {
+  // Check if the circle is colliding with any of the triangle's edges
+  for (let i = 0; i < 3; i++) {
+    let start = triangle[i]
+    let end = triangle[(i + 1) % 3]
+
+    let dx = end.x - start.x
+    let dy = end.y - start.y
+    let length = Math.sqrt(dx * dx + dy * dy)
+
+    let dot =
+      ((circle.coordinates.x - start.x) * dx +
+        (circle.coordinates.y - start.y) * dy) /
+      Math.pow(length, 2)
+
+    let closestX = start.x + dot * dx
+    let closestY = start.y + dot * dy
+
+    if (!isPointOnLineSegment(closestX, closestY, start, end)) {
+      closestX = closestX < start.x ? start.x : end.x
+      closestY = closestY < start.y ? start.y : end.y
+    }
+
+    dx = closestX - circle.coordinates.x
+    dy = closestY - circle.coordinates.y
+
+    let distance = Math.sqrt(dx * dx + dy * dy)
+
+    if (distance <= circle.radius) {
+      return true
+    }
+  }
+
+  // No collision
+  return false
+}
+
+function isPointOnLineSegment(x, y, start, end) {
+  return (
+    x >= Math.min(start.x, end.x) &&
+    x <= Math.max(start.x, end.x) &&
+    y >= Math.min(start.y, end.y) &&
+    y <= Math.max(start.y, end.y)
+  )
+}
+
 
 ///// Main /////
 const MOVEMENT_SPEED = 6.5;
